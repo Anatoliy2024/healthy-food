110df1752274fa5d3eca6f2cdfeaecd2c1455ae5
Anatoliy2024 <gredlend20@gmail.com>
2025-12-26 02:48:52 +0300

    add animation fruit in headerMain

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,44 @@
 import { Navbar } from "../Navbar/Navbar"
 import { HeaderMain } from "../HeaderMain/HeaderMain"
 import style from "./Header.module.scss"
+import Image from "next/image"
 export function Header() {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
         <Navbar />
         <HeaderMain />
+        <div className={style.backgroundFruit}>
+          <div className={style.appleOne}>
+            <Image
+              src="/image/apple.png"
+              width={100}
+              height={100}
+              alt="apple"
+            />
+          </div>
+          <div className={style.appleTwo}>
+            <Image src="/image/apple.png" width={70} height={70} alt="apple" />
+          </div>
+          <div className={style.appleThree}>
+            <Image src="/image/apple.png" width={80} height={80} alt="kiwi" />
+          </div>
+          <div className={style.kiwiOne}>
+            <Image src="/image/kiwi.png" width={100} height={100} alt="kiwi" />
+          </div>
+          <div className={style.kiwiTwo}>
+            <Image src="/image/kiwi.png" width={120} height={120} alt="kiwi" />
+          </div>
+          <div className={style.kiwiThree}>
+            <Image src="/image/kiwi.png" width={180} height={180} alt="kiwi" />
+          </div>
+          <div className={style.pearOne}>
+            <Image src="/image/pear.png" width={120} height={120} alt="pear" />
+          </div>
+          <div className={style.pearTwo}>
+            <Image src="/image/pear.png" width={200} height={200} alt="pear" />
+          </div>
+        </div>
       </div>
     </div>
   )
