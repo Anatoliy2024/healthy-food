b2b3e12f1e273b471dcbd14f54de16750691147c
Anatoliy2024 <gredlend20@gmail.com>
2025-12-27 09:49:40 +0300

    change option next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
-export default nextConfig;
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/healthy-food",
+  assetPrefix: "/healthy-food/",
+}
+
+export default nextConfig
