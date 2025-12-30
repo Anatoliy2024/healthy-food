2f596b8aeb6015f5806e88ce88ff98c7a168b321
Anatoliy2024 <gredlend20@gmail.com>
2025-12-30 06:42:53 +0300

    change navbar a

--- src/components/NavbarList/NavbarList.tsx
+++ src/components/NavbarList/NavbarList.tsx
@@ -17,20 +17,30 @@ export function NavbarList({
 
   return (
     <ul className={`${style.navbar} ${!isOpen ? style.hidden : ""}`}>
-      <li onClick={handleLinkClick}>
-        <a href="#advise">Советы</a>
+      <li>
+        <a onClick={handleLinkClick} href="#advise">
+          Советы
+        </a>
       </li>
-      <li onClick={handleLinkClick}>
-        <a href="#recipe">Рецепты</a>
+      <li>
+        <a onClick={handleLinkClick} href="#recipe">
+          Рецепты
+        </a>
       </li>
-      <li onClick={handleLinkClick}>
-        <a href="#advantage">Индивидуальный подход</a>
+      <li>
+        <a onClick={handleLinkClick} href="#advantage">
+          Индивидуальный подход
+        </a>
       </li>
-      <li onClick={handleLinkClick}>
-        <a href="#templates">Шаблоны</a>
+      <li>
+        <a onClick={handleLinkClick} href="#templates">
+          Шаблоны
+        </a>
       </li>
-      <li onClick={handleLinkClick}>
-        <a href="#worth">Ценности</a>
+      <li>
+        <a onClick={handleLinkClick} href="#worth">
+          Ценности
+        </a>
       </li>
       {/* <li>Советы</li> 
        <li>Рецепты</li>
